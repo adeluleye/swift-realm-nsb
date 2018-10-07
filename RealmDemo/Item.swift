f95d4f67d27fd4e220a059ff03e6a94ee4be52ed
adeluleye <adelu4real@gmail.com>
2018-10-08 00:31:06 +0100

    create a file to handle realm item object

---
+++ RealmDemo/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  RealmDemo
+//
+//  Created by ADELU ABIDEEN ADELEYE on 10/8/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var picture = "todo"
+}
